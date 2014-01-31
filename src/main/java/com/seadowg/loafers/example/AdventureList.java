7b72aa3104dae9d1244598bac761e82a267c088a
Callum Stott <callum@seadowg.com>
2014-01-31 20:02:43 +0000

    Initial commit :metal:

---
+++ src/main/java/com/seadowg/loafers/example/AdventureList.java
@@ -0,0 +1,30 @@
+package com.seadowg.loafers.example;
+
+import com.seadowg.loafers.app.App;
+import com.seadowg.loafers.widget.Button;
+import com.seadowg.loafers.widget.Input;
+import com.seadowg.loafers.widget.List;
+
+public class AdventureList extends App {
+
+  @Override
+  public void open() {
+    setTitle("Adventure List");
+
+    final List list = new List() {
+      @Override
+      public void press(String grocery) {
+        remove(grocery);
+      }
+    };
+
+    final Input input = new Input();
+    new Button("Add") {
+      @Override
+      public void press() {
+        list.add(input.getText());
+        input.clear();
+      }
+    };
+  }
+}
---
+++ src/main/java/com/seadowg/loafers/example/AdventureListApplication.java
@@ -0,0 +1,7 @@
+package com.seadowg.loafers.example;
+
+import android.app.Application;
+
+public class AdventureListApplication extends Application {
+
+}
