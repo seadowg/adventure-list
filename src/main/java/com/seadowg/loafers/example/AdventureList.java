b07ef866a22829198c6cdca908dee0f7ea67ff38
Callum Stott <callum@seadowg.com>
2014-05-18 23:11:10 +0100

    Overrides are probably bad here

--- src/main/java/com/seadowg/loafers/example/AdventureList.java
+++ src/main/java/com/seadowg/loafers/example/AdventureList.java
@@ -6,13 +6,10 @@ import com.seadowg.loafers.widget.Input;
 import com.seadowg.loafers.widget.List;
 
 public class AdventureList extends App {
-
-  @Override
   public void open() {
     setTitle("Adventure List");
 
     final List list = new List() {
-      @Override
       public void press(String item) {
         remove(item);
       }
@@ -20,7 +17,6 @@ public class AdventureList extends App {
 
     final Input input = new Input();
     new Button("Add") {
-      @Override
       public void press() {
         list.add(input.getText());
         input.clear();
